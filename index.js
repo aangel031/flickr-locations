6acaeade42876adac95beda767d818423ea7afe4
Angel <acostaangel031@gmail.com>
2016-07-07 18:48:24 +0000

    Project Upload

---
+++ index.js
@@ -0,0 +1,15 @@
+ /*global $*/
+ 
+ var title = $("#postTitle");
+ var img = $("#postImg");
+ var date = $("#postDate");
+ var location = $("#postLocation");
+ 
+ /*Key: bffb5e308411dc54e09ec03aad9ad61f */
+ /*Secret:d814ce6ea1de5fad */
+ 
+ $("#btnGo").click(function(){
+    var search = $("#txtInput").val();
+    
+    
+ });
